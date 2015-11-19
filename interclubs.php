e113f48de9698a779f6a3a8e29c58677f7e08b3d
zirby <capella@chzirbes.be>
2015-11-19 11:50:32 +0100

    interclub

---
+++ interclubs.php
@@ -0,0 +1,17 @@
+<?php require 'inc/header.php'; ?>
+
+
+    <div class="container">
+      <div class="row center">
+        <h5 class="header col s12 ">Je souhaite faire partie d'une équipe d'Interclub</h5>
+      </div>
+      <div class="row center">
+          <form action="" method="POST">
+                <button type="reset" id="interclub_non" class="btn-large waves-effect waves-light orange">NON</button>
+                <button type="submit" id="interclub_oui" class="btn-large waves-effect waves-light green">OUI</button>
+          </form>
+      </div>
+    </div>
+
+
+<?php require 'inc/footer.php';
